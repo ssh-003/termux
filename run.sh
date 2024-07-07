c4f66a39a37b26fb9ff30d6f1f9a1b350a7cc91d
Arjun_Sharma <ssh.00000008@gmail.com>
2024-07-07 09:00:03 +0545

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,34 @@
+RED='\033[1;31m'
+NC='\033[0m' # No Color
+
+# Print the ASCII art with red color
+echo -e "${RED}
+             00000   00    00  00000000  
+                00   00    00        00
+                00   00    00        00
+                00   00000000  00000000  
+                00         00        00  
+                00         00        00
+               0000        00  00000000
+${NC}"
+
+cd test_languages
+php test.php
+python3 test.py
+ruby test.rb
+node test.js
+sh test.sh
+javac Test.java
+java Test
+perl test.pl
+./test_c 
+./test_cpp 
+echo "Html is installade"
+echo "Css is installed"
+echo "javaScript is installed"
+echo "//-------------------–-------–--------------------------/"
+echo "(Copy    https://localhost:8000    and Goto web browser)"
+echo "
+
+"
+php -S localhost:8000
